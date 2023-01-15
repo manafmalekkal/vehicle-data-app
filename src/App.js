2f7921152160821e7e84c5e9303856544b4b8e1a
manafmalekkal <manafpeeyem@gmail.com>
2023-01-15 22:08:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const[datas,setDatas]=useState([]);
+  const[filterInput,setFilterInput]=useState('');
+  const[error,setError]=useState('');
+  const[filteredData,setFilteredData]=useState([]);
+
+  
+  const inputHandler=(e)=>{
+    setFilterInput(e.target.value);
+  }
+
+  const buttonHandler=()=>{
+
+    console.log('btn click');
+    let filterArray=[];
+    filterArray= datas?.filter((res)=>{return res.Country===filterInput.toUpperCase()});
+    setFilterInput('');
+    setFilteredData(filterArray);
+    if(filterArray.length===0) {setError('No Results Found'); setFilterInput('');}
+  }
+
+  useEffect(()=>{
+    fetch('https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json')
+    .then((res)=>res.json())
+    .then((data)=>{setDatas(data.Results)})
+  },[]);
+
+  const vehicleData = (data)=>{ 
+    return(
+    data?.map((items)=>
+    <>
+    {
+     ( items.VehicleTypes.length ? 
+   ( items.VehicleTypes.map((type)=>
+   <tr>
+       <td >{type.Name}</td>
+       <td >{items.Country}</td>
+       <td >{items.Mfr_Name}</td>
+       <td >{items.Mfr_CommonName}</td>
+   </tr>) ) : (<tr>
+    <td></td>
+       <td >{items.Country}</td>
+       <td >{items.Mfr_Name}</td>
+       <td >{items.Mfr_CommonName}</td>
+   </tr> ))
+ 
+ }
+ </>
+ ))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vehicle List</h1>
+      <input value={filterInput} onChange={inputHandler} placeholder='Enetr country Name'/>
+      <button onClick={buttonHandler}>Filter</button>
+      <table>
+        <thead>
+        <tr>
+          <th>Vehicle Type</th>
+          <th>Country</th>
+          <th>Name</th>
+          <th>mfr_commonName</th>
+        </tr>
+        
+        </thead>
+        <tbody>
+        {
+         filteredData.length? vehicleData(filteredData) : vehicleData(datas)
+        }
+        </tbody>
+      </table>
+      {error? <div>{error}</div>:''}
+      {/* {datas['Results'] && datas['Results'][0]['VehicleTypes'][1]['Name']} */}
+
     </div>
   );
 }
